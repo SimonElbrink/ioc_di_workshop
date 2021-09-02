4b495e4f3c803801d15eab6b03dcaf2e2c8a6a8c
Simon Elbrink <simon.elbrink@gmail.com>
2021-09-02 10:43:02 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/workshop/AppTest.java
@@ -0,0 +1,21 @@
+package se.lexicon.workshop;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
